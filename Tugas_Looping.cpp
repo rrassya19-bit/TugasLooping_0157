02a66f7c4cbacc1a98187367ceabcbcaa2861a0f
rrassya19-bit <rrassys19@gmail.com>
2026-03-31 17:13:13 +0700

    Tambah while loop loncat bilangan ganjil di isPrima

--- Tugas_Looping.cpp
+++ Tugas_Looping.cpp
@@ -69,5 +69,10 @@ bool isPrima(int n) {
     if (n <= 1) return false;
     if (n == 2) return true;
     if (n % 2 == 0) return false;
+    int i = 3;
+    while (i * i <= n) {
+        if (n % i == 0) return false;
+        i += 2;
+    }
     return true;
 }
